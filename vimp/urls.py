6e96936dec510c6ce667378a1c2e17024d461143
Kevin Yang <kuan_yang@mfe.berkeley.edu>
2014-01-19 12:11:06 -0800

    Add fabfile.py

--- vimp/urls.py
+++ vimp/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from django.views.generic import RedirectView
 from django.contrib import admin
 admin.autodiscover()
 
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ejub.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', RedirectView.as_view(url='/vm/')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^vm/', include('vm.urls', namespace = 'vm')),
 )
