beabe7b01002f8ab458b3da8b546f9e1aca71cc7
Kevin Yang <kuan_yang@mfe.berkeley.edu>
2014-01-20 13:56:29 -0800

    Add map to the precinct detail page

--- vm/static/js/precinct_detail.js
+++ vm/static/js/precinct_detail.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
     $('.individual-voter').mouseenter(function() {
         $('.individual-voter').removeClass("voter_highlight");
         $(this).addClass("voter_highlight");
@@ -7,4 +8,28 @@ $(document).ready(function() {
     $(".individual-voter").mouseleave(function() {
         $(this).removeClass("voter_highlight");
     });
+
+    // If click on the voter, load the map info and voting history
+    $('.individual-voter').click(function() {
+        var address = $(this).attr("voter-address");
+        var city = $(this).attr("voter-city");
+        var state = $(this).attr("voter-state");
+        var zipcode = $(this).attr("voter-zipcode");
+        
+    });
+
+    // Javascript for map initialization
+    var geoXmlDoc;
+    var map;
+    var info_window = null;
+    function initialize() {
+        var mapOptions = {
+            center: new google.maps.LatLng(35.321394, -119.016564),
+zoom: 10
+        };
+        map = new google.maps.Map(document.getElementById("map-canvas-voter"), mapOptions);
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+
 });
