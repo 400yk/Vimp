c7a2dc65298d2bcdcfdcfc259a058fb0fd914b1d
Kevin Yang <kuan_yang@mfe.berkeley.edu>
2014-01-20 21:50:43 -0800

    Allow the map to change when click on voter

--- vm/static/js/precinct_detail.js
+++ vm/static/js/precinct_detail.js
@@ -16,16 +16,12 @@ $(document).ready(function() {
         var city = $(this).attr("voter-city");
         var state = $(this).attr("voter-state");
         var zipcode = $(this).attr("voter-zipcode");
-
+        var voter_address_input = document.getElementById("voter-address-input");
         $("#voter-address-input").attr("value", address + ", " + city + ", " + state + ", " + zipcode);
 
         // Move the map to show the location of the voter
-           input = /** @type {HTMLInputElement} */(
-                document.getElementById('voter-address-input'));
-    // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-         var searchBox = new google.maps.places.SearchBox(
-                /** @type {HTMLInputElement} */(input)); 
+        google.maps.event.trigger(voter_address_input, 'focus');
+        google.maps.event.trigger(voter_address_input, 'keydown', {keyCode: 13});
     });
 
     // Javascript for map initialization
@@ -33,11 +29,51 @@ $(document).ready(function() {
     var map;
     var info_window = null;
     function initialize() {
+        var markers = []
         var mapOptions = {
             center: new google.maps.LatLng(35.321394, -119.016564),
-zoom: 10
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            zoom: 10
         };
         map = new google.maps.Map(document.getElementById("map-canvas-voter"), mapOptions);
+        input = /** @type {HTMLInputElement} */(
+                document.getElementById('voter-address-input'));
+
+        var searchBox = new google.maps.places.SearchBox(
+                /** @type {HTMLInputElement} */(input)); 
+        google.maps.event.addListener(searchBox, 'places_changed', function() {
+            var places = searchBox.getPlaces();
+
+            for (var i = 0, marker; marker = markers[i]; i++) {
+                marker.setMap(null);
+            }
+
+            markers = [];
+            var bounds = new google.maps.LatLngBounds();
+            for (var i = 0, place; place = places[i]; i++) {
+                var image = {
+                    url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(25, 25)
+                };
+
+                var marker = new google.maps.Marker({
+                    map: map,
+                    icon: image,
+                    title: place.name,
+                    position: place.geometry.location
+                });
+
+                markers.push(marker);
+
+                bounds.extend(place.geometry.location);
+            }
+
+            map.fitBounds(bounds);
+            map.setZoom(16);
+        });
     }
 
     google.maps.event.addDomListener(window, 'load', initialize);
